a9005872ad38015d29649b8c14882deb91f5fe2e
MOHAMED IBRAHIM <205980490+ENG-mohamedibrahiem@users.noreply.github.com>
2026-06-25 20:48:58 +0300

    feat(domain): add tplink to RouterBrand enum

--- lib/features/routers/domain/entities/router_brand.dart
+++ lib/features/routers/domain/entities/router_brand.dart
@@ -1,10 +1,12 @@
 enum RouterBrand {
   zte,
+  tplink,
   unknown;
 
   String get displayName {
     return switch (this) {
       RouterBrand.zte => 'ZTE',
+      RouterBrand.tplink => 'TP-Link',
       RouterBrand.unknown => 'Unknown',
     };
   }
